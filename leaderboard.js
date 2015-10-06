84599f29a77c7cec8e46f9b27ab6a68409069fd9
Gregg Hanano <gregg.hanano@gmail.com>
2015-10-06 14:45:25 -0700

    added selection highlighting

--- leaderboard.js
+++ leaderboard.js
@@ -5,14 +5,19 @@ if(Meteor.isClient){
   Template.leaderboard.helpers({
     'player': function(){
       return PlayersList.find();
+    },
+    'selectedClass': function(){
+      var playerId = this._id;
+      var selectedPlayer = Session.get('selectedPlayer');
+      if(playerId === selectedPlayer){
+        return "selected"
+      }
     }
   });
   Template.leaderboard.events({
     'click .player': function(){
       var playerId = this._id;
       Session.set('selectedPlayer', playerId);
-      var selectedPlayer = Session.get('selectedPlayer');
-      console.log(selectedPlayer);
     }
   });
 }
